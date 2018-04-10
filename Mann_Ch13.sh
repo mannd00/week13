39c1e14d73b6b2e4fc6806e9b579ad2505602e0c
mannd00 <35670627+mannd00@users.noreply.github.com>
2018-04-10 16:57:04 -0500

    Update Mann_Ch13.sh

--- Mann_Ch13.sh
+++ Mann_Ch13.sh
@@ -3,3 +3,12 @@
 echo "Welcome to Chapter 13!"
 
 echo -e "\nTopic today is User Environments!"
+
+echo -e "\nDefault locations for command paths are in /usr/local/bin /usr/bin and /bin"
+echo -e "\nLets see whats in there!"
+
+ls -lha /usr/local/bin
+ls -lha /usr/bin
+ls -lha /bin
+
+echo -e "\n Thats where we can find all the commands we can use in Bash"
