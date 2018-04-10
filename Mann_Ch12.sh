5d878869ed30f86a98cf9b106ef4eafa71df9b98
mannd00 <35670627+mannd00@users.noreply.github.com>
2018-04-10 15:17:19 -0500

    Update Mann_Ch12.sh

--- Mann_Ch12.sh
+++ Mann_Ch12.sh
@@ -1,3 +1,14 @@
 #! /bin/bash
 
 echo "Welcome to Chapter 12!"
+
+echo -e "\nThe Topic today is File Transfers."
+ehco -e "\nOne simple way to perform a backup of a directory for example is to use rsync to backup over ssh."
+
+rsync -nva ~/ 127.0.0.1:/mnt/Backups
+
+echo -e "\nRsync also offeres the capibility to compress the files to be transfered usng -z."
+rsync -nvaz ~/ 127.0.0.1:/mnt/Backups
+
+echo -e "\n Rsync also works for pulling files from a remote host to store locally, simply switch the source and desitnation arguments."
+rsync -nvaz 127.0.0.1:/mnt/Backups ~/Backups/
